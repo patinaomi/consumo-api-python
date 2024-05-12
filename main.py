acde7c6456001bd91a174693290ce66479960923
Patricia Naomi <patricianaomiyama@gmail.com>
2024-05-12 03:24:43 -0300

    preparando os dados para o banco

--- main.py
+++ main.py
@@ -4,6 +4,18 @@ import requests
 from flask import Flask, request, jsonify, render_template
 
 
+def validar_telefone(telefone, cel=True):
+    # Remove caracteres não numéricos da string telefone usando a função filter e str.isdigit
+    # e depois o join junta os caracteres restantes de volta em uma string única.
+
+    telefone_limpo = ''.join(filter(str.isdigit, telefone))
+
+    if cel:
+        return telefone_limpo[:2] + '9' + telefone_limpo[2:]
+    else:
+        return telefone_limpo
+
+
 def carregar_dados(dados):
     try:
         with open(dados, 'r') as arquivo:
@@ -12,19 +24,39 @@ def carregar_dados(dados):
     except FileNotFoundError:
         print(f'O arquivo {dados} não foi encontrado.')
         return
+    except Exception as e:
+        print(f'Erro: {e}')
+        return
 
 
 secret = carregar_dados('secret.json')
-usuario = secret['user']  # Usuário
-senha = secret['password']  # Senha
-oracle = secret['dsn']  # Servidor
 
 
-def executar_sql(usuario, senha, oracle, sql):
+def operacao_db(comando, secret, commit=True):
     try:
-        with oracledb.connect(user=usuario, password=senha, dsn=oracle) as connection:
-            cursor = connection.cursor()
-            cursor.execute(sql)
+        with oracledb.connect(user=secret['user'],
+                              password=secret['password'],
+                              dsn=secret['dsn']) as connection:
+            with connection.cursor() as cursor:
+                cursor.execute(comando)
+
+                if commit:
+                    connection.commit()
+
+    except oracledb.DatabaseError as e:
+        print(f'Erro no banco de dados: {e}')
+    except Exception as e:
+        print(f'Erro: {e}')
+
+
+def consulta_db(comando, secret):
+    try:
+        with oracledb.connect(user=secret['user'],
+                              password=secret['password'],
+                              dsn=secret['dsn']) as connection:
+            with connection.cursor() as cursor:
+                cursor.execute(comando)
+                return cursor.fetchall()
 
     except oracledb.DatabaseError as e:
         print(f'Erro no banco de dados: {e}')
@@ -45,6 +77,30 @@ sql_criar_tabela = """
     tel_celular NUMBER(20)
 )"""
 
-# Criando as tabelas
-executar_sql(usuario, senha, oracle, sql_drop_tabela)
-executar_sql(usuario, senha, oracle, sql_criar_tabela)
+# operacao_db(sql_drop_tabela, secret)
+# operacao_db(sql_criar_tabela, secret)
+
+
+qtd_busca = 1
+genero = 'female'
+
+url = (f'https://randomuser.me/api/?results={qtd_busca}&nat=br,&gender={genero},&inc=name,gender,nat,location,dob,'
+       f'email,phone,cell&noinfo')
+
+response = requests.get(url)
+
+# Dados obtidos na consulta inicial
+
+dict_pessoa = response.json()  # Converte a resposta em JSON para um dicionário Python
+
+
+# Deserializar o JSON para um objeto Python
+
+# Preparando os dados para o banco
+nome_completo = dict_pessoa['results'][0]['name']['first'] + ' ' + dict_pessoa['results'][0]['name']['last']
+genero = 'F' if dict_pessoa['results'][0]['gender'] == 'female' else 'M'
+data = dict_pessoa['results'][0]['dob']['date'][:10]  # Pra pegar só a data certinho
+endereco = dict_pessoa['results'][0]['location']['street']['name'] + ' ' + str(dict_pessoa['results'][0]['location']['street']['number'])
+email = dict_pessoa['results'][0]['email']
+tel_residencial = validar_telefone(dict_pessoa['results'][0]['phone'], False)
+tel_celular = validar_telefone(dict_pessoa['results'][0]['cell'])
